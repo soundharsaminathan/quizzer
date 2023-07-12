77ed26720ac14a512393c60091e9de9a4281f689
JeyasuryaDisprz <jeyasurya.r@disprz.com>
2023-07-12 21:44:32 +0530

    transition animation

--- src/Modal2/Mock.jsx
+++ src/Modal2/Mock.jsx
@@ -2,18 +2,23 @@ import Ball from "./Ball";
 import Box from "./Box";
 
 import "../Styles2/Mock.scss";
+import { useState } from "react";
 const Mock = () => {
+    const [isMoving, setMoving] = useState(false);
+    const [isBounce, setBounce ] = useState(false);
 
     return (
         <>
-            <div className="floor" onKeyDown={()=>{
-                
+            <div className="floor" onKeyDown={(event)=>{
+                if(event.key === 'ArrowRight') {
+                    console.log('Move');
+                }
             }}>
-                <Ball />
+                <Ball position={0} isMoving={isMoving} isBounce={isBounce}/>
                 <Box position={[10, 50]} answer={"Option 1"}/>
                 <Box position={[30, 30]} answer={"Option 2"}/>
-                <Box position={[55, 30]} answer={"Option 3"}/>
-                <Box position={[80, 50]} answer={"Option 4"}/>
+                <Box position={[50, 30]} answer={"Option 3"}/>
+                <Box position={[70, 50]} answer={"Option 4"}/>
             </div>
         </>
     )
