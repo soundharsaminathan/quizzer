86effa9f9e1156a3784508289ca1f95365b63c9a
JeyasuryaDisprz <jeyasurya.r@disprz.com>
2023-07-12 17:38:52 +0530

    change graound and qst

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -3,21 +3,26 @@ import { Environment, OrbitControls } from "@react-three/drei"
 import Options from "../Views/Options"
 import Question from "../Views/Question"
 import HealthMeter from "../Modals/HealthMeter"
+import "../Styles/Quiz.scss"
 
 // import styles from '../Quiz.scss'
 
 const Quiz = () => {
     return(
         <>
-            <Canvas orthographic camera={{ position: [0, 0, 16], top:16, bottom:-16, left:16, right:-16, zoom:50 }}>
-                <ambientLight intensity={0.3}  />
-              
-                <pointLight position={[10, 8, 0]} color={"white"} />
-                {/* <Environment preset="warehouse" background /> */}
-                <Options/>
-            </Canvas>
-            <Question/>
-            <HealthMeter />
+            <div className="canvas">
+                <Canvas orthographic camera={{ position: [0, 0, 16], top:16, bottom:-16, left:16, right:-16, zoom:50 }}>
+                    <ambientLight intensity={0.3}  />
+
+                    <pointLight position={[10, 8, 0]} color={"white"} />
+                    {/* <Environment preset="warehouse" background /> */}
+                    <Options/>
+                </Canvas>
+                <HealthMeter />
+            </div>
+            <div>
+                <Question/>
+            </div>
         </>
     )
 }
