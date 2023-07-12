509dceb87d15471ad89bd64ada9823a7f78df73a
soundhar.s <soundhar.s@disprz.com>
2023-07-12 21:47:48 +0530

    adding key press

--- src/Modal2/Mock.jsx
+++ src/Modal2/Mock.jsx
@@ -2,13 +2,19 @@ import Ball from "./Ball";
 import Box from "./Box";
 
 import "../Styles2/Mock.scss";
+import { useEffect, useRef } from "react";
 const Mock = () => {
+    const ref = useRef(null);
+    useEffect(()=>{
+        document.addEventListener('keydown',detectKey, true);
+    })
+    const detectKey = (e) =>{
+        console.log(e.key,"hh",ref.current.offsetWidth)
 
+    }
     return (
         <>
-            <div className="floor" onKeyDown={()=>{
-                
-            }}>
+            <div ref={ref} className="floor">
                 <Ball />
                 <Box position={[10, 50]} answer={"Option 1"}/>
                 <Box position={[30, 30]} answer={"Option 2"}/>
