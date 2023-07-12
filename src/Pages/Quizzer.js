334aaf0b3432a6f8ee11b598a8bb234e84b19681
Ajay <ajay.r@disprz.com>
2023-07-12 14:47:22 +0530

    quiz instruction

--- src/Pages/Quizzer.js
+++ src/Pages/Quizzer.js
@@ -15,6 +15,15 @@ const Quizzer = () => {
                     <button>Start</button>
                     <button className='btn-leaderboard' disabled={true}>Leaderboard</button>
                 </div>
+                <div className='instruction-container'>
+                    <h1>Instructions</h1>
+                    <ul>
+                        <li>This is a timed quiz spanning 15 minutes</li>
+                        <li>There are a total of 20 questions</li>
+                        <li>Once you submit an answer to a question, you cannot reattempt the same</li>
+                        <li>The quiz will end as soon as you deplete all your energy</li>
+                    </ul>
+                </div>
             </div>
         </>
     )
