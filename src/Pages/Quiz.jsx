16140c81dd759a713cae0006136946c5d46ec4b6
Ajay <ajay.r@disprz.com>
2023-07-12 19:41:42 +0530

    timer counter

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -1,16 +1,20 @@
 import { Canvas } from "@react-three/fiber"
-import { Environment, OrbitControls } from "@react-three/drei"
 import Options from "../Views/Options"
 import Question from "../Views/Question"
 import HealthMeter from "../Modals/HealthMeter"
 import "../Styles/Quiz.scss"
+import Timer from "../Modals/Timer"
 
 // import styles from '../Quiz.scss'
 
 const Quiz = () => {
+
     return(
         <>
             <div className="canvas">
+                <div className="time-counter">
+                    <Timer delayResend="899"/>
+                </div>
                 <Canvas orthographic camera={{ position: [0, 0, 16], top:16, bottom:-16, left:16, right:-16, zoom:50 }}>
                     <ambientLight intensity={0.3}  />
 
@@ -20,9 +24,7 @@ const Quiz = () => {
                 </Canvas>
                 <HealthMeter />
             </div>
-            <div>
-                <Question/>
-            </div>
+            <div><Question/></div>
         </>
     )
 }
