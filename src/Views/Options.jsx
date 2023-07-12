584da7549d7704b422c78c8f244a4e1e178221cc
JeyasuryaDisprz <jeyasurya.r@disprz.com>
2023-07-12 12:15:12 +0530

    question added

--- src/Views/Options.jsx
+++ src/Views/Options.jsx
@@ -1,12 +1,13 @@
 import { Physics } from "@react-three/rapier"
 import Ball from "../Modals/Ball";
 import Ground from "../Modals/Ground";
+import Brick from "../Modals/Brick";
 
 const Options = () =>{
     return (
     <Physics gravity={[0, -30, 0]}>
         <Ball />
-        {/* // <Brick color="pink" position={[-12, 3.5, 0]} /> */}
+        <Brick />
         <Ground />
     </Physics>
     );
