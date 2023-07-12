8c8009bfd8e9b70ca6bcfdbaaaa57db674a4ae33
JeyasuryaDisprz <jeyasurya.r@disprz.com>
2023-07-12 21:00:36 +0530

    spin animation

--- src/Modal2/Mock.jsx
+++ src/Modal2/Mock.jsx
@@ -3,13 +3,16 @@ import Box from "./Box";
 
 import "../Styles2/Mock.scss";
 const Mock = () => {
+
     return (
         <>
-            <div className="floor">
+            <div className="floor" onKeyDown={()=>{
+                
+            }}>
                 <Ball />
                 <Box position={[10, 50]} answer={"Option 1"}/>
-                <Box position={[30, 50]} answer={"Option 2"}/>
-                <Box position={[55, 50]} answer={"Option 3"}/>
+                <Box position={[30, 30]} answer={"Option 2"}/>
+                <Box position={[55, 30]} answer={"Option 3"}/>
                 <Box position={[80, 50]} answer={"Option 4"}/>
             </div>
         </>
