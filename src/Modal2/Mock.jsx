c4616e900907a706f4160ed5fc5b1206403a55d4
soundhar.s <soundhar.s@disprz.com>
2023-07-13 11:27:35 +0530

    multiselect

--- src/Modal2/Mock.jsx
+++ src/Modal2/Mock.jsx
@@ -11,7 +11,7 @@ const Mock = ({ questionNumber }) => {
    const [position, setPosition] = useState(0);
    const [isKeyDown, setIsKeyDown] = useState(false);
    const [isSelected, setSelected] = useState(false);
-   const [selectedOption, setSelectedOption] = useState(0);
+   const [selectedOptions, setSelectedOptions] = useState([]);
    const [bounce, setBounce] = useState(false)
    const [iBounce, setIsBounce] = useState(false)
 
@@ -39,16 +39,32 @@ const Mock = ({ questionNumber }) => {
       console.log(position,width,"pow");
       if (position >= width/5 && position < width*2/5) {
          setSelected(!isSelected);
-         setSelectedOption(1);
+         if(selectedOptions.includes(1)){
+            setSelectedOptions([...selectedOptions.filter(op=>op != 1)])
+        } else {
+             setSelectedOptions([...selectedOptions,1])
+         }
       } else if (position >= width*2/5 && position < width*3/5) {
          setSelected(!isSelected);
-         setSelectedOption(2);
+         if(selectedOptions.includes(2)){
+            setSelectedOptions([...selectedOptions.filter(op=>op != 2)])
+        } else {
+             setSelectedOptions([...selectedOptions,2])
+         }
       }else if (position >= width*3/5 && position < width*4/5){
          setSelected(!isSelected);
-         setSelectedOption(3);
+         if(selectedOptions.includes(3)){
+            setSelectedOptions([...selectedOptions.filter(op=>op != 3)])
+        } else {
+             setSelectedOptions([...selectedOptions,3])
+         }
       }else if (position >= width*4/5 && position < width*4.5/5){
          setSelected(!isSelected);
-         setSelectedOption(4);
+         if(selectedOptions.includes(4)){
+            setSelectedOptions([...selectedOptions.filter(op=>op != 4)])
+        } else {
+             setSelectedOptions([...selectedOptions,4])
+         }
       } else {
          setSelected(isSelected);
       }
@@ -89,13 +105,13 @@ const Mock = ({ questionNumber }) => {
                index == 0 || index == questions[questionNumber].options?.length - 1
                   ? 50
                   : 30;
-            let isSelected = selectedOption === index+1;
-            console.log(selectedOption, " isS");
+            let isSelected = selectedOptions.includes(index+1);
+            console.log(selectedOptions, " isS");
             return (
                <Box
                   position={[index * 20 + 15, y]}
                   option={option}
-                  isSelected={selectedOption === index+1}
+                  isSelected={isSelected}
                />
             );
          })}
