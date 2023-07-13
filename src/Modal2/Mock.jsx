d9175519b8e0c212b29f21300cabd8bd82955da8
soundhar.s <soundhar.s@disprz.com>
2023-07-13 11:22:09 +0530

    fix

--- src/Modal2/Mock.jsx
+++ src/Modal2/Mock.jsx
@@ -7,113 +7,106 @@ import Question from "../Views/Question";
 import { questions } from "../Questions/QuestionBank";
 
 const Mock = ({ questionNumber }) => {
-    const ref = useRef(null);
-    const [position, setPosition] = useState(0);
-    const [isKeyDown, setIsKeyDown] = useState(false);
-    const [isSelected, setSelected] = useState(false);
-    const [selectedOption, setSelectedOption] = useState(0);
+   const ref = useRef(null);
+   const [position, setPosition] = useState(0);
+   const [isKeyDown, setIsKeyDown] = useState(false);
+   const [isSelected, setSelected] = useState(false);
+   const [selectedOption, setSelectedOption] = useState(0);
+   const [bounce, setBounce] = useState(false)
+   const [iBounce, setIsBounce] = useState(false)
 
-    const handleSection = () => {
-        const width = ref.current?.offsetWidth;
-        console.log(width / 10, " 10 perc width", width / 10 + 100, " 10 p + 110");
-        console.log(
-            (width * 3) / 10,
-            " 10 perc width",
-            (width * 3) / 10 + 100,
-            " 10 p + 110"
-        );
-        console.log(
-            (width * 5) / 10,
-            " 10 perc width",
-            (width * 5) / 10 + 100,
-            " 10 p + 110"
-        );
-        console.log(
-            (width * 7) / 10,
-            " 10 perc width",
-            (width * 7) / 10 + 100,
-            " 10 p + 110"
-        );
-        console.log(isSelected);
-        if (position >= width / 10 && position < width / 10 + 110) {
-            setSelected(!isSelected);
-            setSelectedOption(1);
-        } else if (
-            position >= (width * 3) / 10 &&
-            position < (width * 3) / 10 + 100
-        ) {
-            setSelected(!isSelected);
-            setSelectedOption(2);
-        } else if (
-            position >= (width * 5) / 10 &&
-            position < (width * 5) / 10 + 100
-        ) {
-            setSelected(!isSelected);
-            setSelectedOption(3);
-        } else if (
-            position >= (width * 7) / 10 &&
-            position < (width * 7) / 10 + 100
-        ) {
-            setSelected(!isSelected);
-            setSelectedOption(4);
-        } else {
-            setSelected(isSelected);
-        }
-    };
+   const handleSection = () => {
+      const width = ref.current?.offsetWidth;
+      console.log(width / 10, " 10 perc width", width / 10 + 100, " 10 p + 110");
+      console.log(
+         (width * 3) / 10,
+         " 10 perc width",
+         (width * 3) / 10 + 100,
+         " 10 p + 110"
+      );
+      console.log(
+         (width * 5) / 10,
+         " 10 perc width",
+         (width * 5) / 10 + 100,
+         " 10 p + 110"
+      );
+      console.log(
+         (width * 7) / 10,
+         " 10 perc width",
+         (width * 7) / 10 + 100,
+         " 10 p + 110"
+      );
+      console.log(position,width,"pow");
+      if (position >= width/5 && position < width*2/5) {
+         setSelected(!isSelected);
+         setSelectedOption(1);
+      } else if (position >= width*2/5 && position < width*3/5) {
+         setSelected(!isSelected);
+         setSelectedOption(2);
+      }else if (position >= width*3/5 && position < width*4/5){
+         setSelected(!isSelected);
+         setSelectedOption(3);
+      }else if (position >= width*4/5 && position < width*4.5/5){
+         setSelected(!isSelected);
+         setSelectedOption(4);
+      } else {
+         setSelected(isSelected);
+      }
+   };
 
-    useEffect(()=>{
-        document.addEventListener('keydown',detectKey, true);
-        document.addEventListener('keyup',releaseKey, true);
-    })
+   useEffect(() => {
+      document.addEventListener("keydown", detectKey, true);
+      document.addEventListener("keyup", releaseKey, true);
+   });
 
-    const detectKey = (e) =>{
-        if(isKeyDown){
-            return;
-        }
-        const width = ref.current?.offsetWidth;
-        if (e.key == "ArrowRight" && position + 100 < width) {
-            setPosition(position + width * 20/100);
-        } else if (e.key == "ArrowLeft" && position > width / 10) {
-            setPosition(position - width * 20/100);
-        } else if (e.key == "ArrowUp") {
-            console.log("hh", ref.current?.offsetWidth, position);
-            handleSection();
-        }
-        setIsKeyDown(true)
-    }
-    const releaseKey = () => {
-        setIsKeyDown(false)
-    }
-    return (
-        <>
-            {" "}
-            <div ref={ref} className="floor">
-                <Ball position={position} /> {" "}
-            </div>
-            {" "}
-            {questions[questionNumber].options?.map((option, index) => {
-                let y =
-                    index == 0 || index == questions[questionNumber].options?.length - 1
-                        ? 50
-                        : 30;
-                let isSelected = selectedOption === index+1;
-                console.log(selectedOption, " isS");
-                return (
-                    <Box
-                        position={[index * 20 + 15, y]}
-                        option={option}
-                        isSelected={selectedOption === index+1}
-                    />
-                );
-            })}
-            {" "}
-            {/* <Box position={[30, 30]} answer={"Option 2"} isSelected={isSelected}/>
-            <Box position={[50, 30]} answer={"Option 3"} isSelected={isSelected}/>
-            <Box position={[70, 50]} answer={"Option 4"} isSelected={isSelected}/> */}
-            <Question question={questions[questionNumber].question} />
-            {" "}
-        </>
-    );
+   const detectKey = (e) => {
+      if (isKeyDown) {
+         return;
+      }
+      const width = ref.current?.offsetWidth;
+      if (e.key == "ArrowRight" && position + 100 < width) {
+         setPosition(position + width * 20/100);
+      } else if (e.key == "ArrowLeft" && position > width / 10) {
+         setPosition(position - width * 20/100);
+      } else if (e.key == "ArrowUp") {
+         console.log("hh", ref.current?.offsetWidth, position);
+         handleSection();
+      }
+      setIsKeyDown(true);
+   };
+   const releaseKey = () => {
+      setIsKeyDown(false);
+   };
+   return (
+      <>
+         {" "}
+         <div ref={ref} className="floor">
+            <Ball position={position} bounce setBounce={setBounce} setIsBounce ={setIsBounce}/> {" "}
+         </div>
+         {" "}
+         {questions[questionNumber].options?.map((option, index) => {
+            let y =
+               index == 0 || index == questions[questionNumber].options?.length - 1
+                  ? 50
+                  : 30;
+            let isSelected = selectedOption === index+1;
+            console.log(selectedOption, " isS");
+            return (
+               <Box
+                  position={[index * 20 + 15, y]}
+                  option={option}
+                  isSelected={selectedOption === index+1}
+               />
+            );
+         })}
+         {" "}
+         {/* <Box position={[30, 30]} answer={"Option 2"} isSelected={isSelected}/>
+         <Box position={[50, 30]} answer={"Option 3"} isSelected={isSelected}/>
+         <Box position={[70, 50]} answer={"Option 4"} isSelected={isSelected}/> */}
+         <Question question={questions[questionNumber].question} />
+         {" "}
+      </>
+   );
 };
 
-export default Mock;
+export default Mock
