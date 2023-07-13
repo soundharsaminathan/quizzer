6de8831b42eba7686d4b3301f4f3b015fed66cb2
Ajay <ajay.r@disprz.com>
2023-07-13 11:14:33 +0530

    Next Question arrow

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -7,30 +7,30 @@ import Mock from "../Modal2/Mock"
 import Timer from "../Modals/Timer"
 import { questions } from "../Questions/QuestionBank"
 import { useState } from "react"
+import arrowImage from "../Resources/arrowImage.png"
+
 
 // import styles from '../Quiz.scss'
 
 const Quiz = () => {
     const [questionNumber, setQuestionNumber] = useState(0);
+    const[gameState, setGameSet] = useState("");
     return(
         <>
+            <div className={`red-bg-${gameState}`}></div>
             <div className="canvas">
                 <div className="time-counter">
-                    {/* <Timer delayResend="899"/> */}
+                    <Timer delayResend="899"/>
                 </div>
-                {/* <Canvas orthographic camera={{ position: [0, 0, 16], top:16, bottom:-16, left:16, right:-16, zoom:50 }}>
-                    <ambientLight intensity={0.3}  />
-
-                    <pointLight position={[10, 8, 0]} color={"white"} />
-                    <Options/>
-                </Canvas> */}
-                        
                     <Mock questionNumber={questionNumber} />
-                    <div style={{position: "absolute", bottom: "50px", zIndex: "10" ,right:"10px", display: "flex", gap:"20px"}}>
+                    {/* <div style={{position: "absolute", bottom: "50px", zIndex: "10" ,right:"10px", display: "flex", gap:"20px"}}>
                         <button onClick={()=>{setQuestionNumber(0)}}>prev</button>
                         <button onClick={()=>{setQuestionNumber(1)}}>next</button>
-                    </div>
-                {/* <HealthMeter /> */}
+                    </div> */}
+                <HealthMeter />
+                <div className="arrow-image-container" onClick={()=>{setQuestionNumber(1)}}>
+                    <img src={arrowImage} className='arrow-img'></img>
+                </div>
             </div>
         </>
     )
